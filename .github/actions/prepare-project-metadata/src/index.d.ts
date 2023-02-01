17d5eb92004c593401cffe635cee0483c16479fa
Anton <antonlashkov@sumoscheduler.com>
2023-02-01 12:35:11 +0300

    make deploy workflow simplier

--- .github/actions/prepare-project-metadata/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'template-file' {
-  /* eslint-disable  @typescript-eslint/no-explicit-any */
-  function renderString(template: string, data: any): string
-  function renderTemplateFile(
-    filepath: string | Buffer | URL,
-    data: any
-  ): Promise<string>
-}
